c4f7fa0864088df6c0ea55c8a0ee4d3d0d1ff3b3
Kryptix <70448046+KryptGit@users.noreply.github.com>
2024-09-08 17:05:21 -0500

    create react app

---
+++ FrontEnd/frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import './Button'
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Button props='test'/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/frontend/src/Button.js
@@ -0,0 +1,12 @@
+function Button(props) {
+  
+    return(
+    <>
+
+        <button>{props.label}</button>
+
+    </>
+    );
+}
+
+export default Button;
