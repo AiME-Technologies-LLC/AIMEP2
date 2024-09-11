712d63effef873ee0d30692e71982ced3d42b558
Kryptix <70448046+KryptGit@users.noreply.github.com>
2024-09-10 22:36:07 -0500

    REFACTOR

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import './Button'
 import Button from './Button';
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
